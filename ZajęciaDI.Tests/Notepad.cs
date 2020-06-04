f6591a43afb0bd1107fd007e38a500948e82b22a
Paweł Ślusarczyk <p.slusarczyk@necto.com.pl>
2020-06-04 17:00:37 +0200

    initial commit

---
+++ ZajęciaDI.Tests/Notepad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Autofac;
+using NUnit.Framework;
+using ZajeciaDi;
+
+namespace ZajęciaDI.Tests
+{
+    public class Notepad
+    {
+        [Test]
+        public void Test()
+        {
+            
+        }
+
+    }
+}
