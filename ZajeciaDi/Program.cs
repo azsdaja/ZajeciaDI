2cf409dde99df1de588b4111bf4fbaa96d288556
Paweł Ślusarczyk <p.slusarczyk@necto.com.pl>
2020-06-04 21:20:43 +0200

    introduced DI with Autofac instead of poor man's DI

--- ZajeciaDi/Program.cs
+++ ZajeciaDi/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
+using Autofac;
 
 namespace ZajeciaDi
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
 
-            var realFacebookService = new FacebookService();
+            // can't create it now like that!
+            // var realFacebookService = new FacebookService();
 
-            
+            var containerBuilder = CreateBasicContainerBuilder();
+            IContainer container = containerBuilder.Build();
+
+            FacebookService realFacebookService = container.Resolve<FacebookService>();
+
+
+        }
+
+        public static ContainerBuilder CreateBasicContainerBuilder()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            //               twórz taki typ          gdy potrzebny ten interfejs       korzystaj z tej samej instancji
+            containerBuilder.RegisterType<Censor>().As<ICensor>().SingleInstance();
+            containerBuilder.RegisterType<FacebookService>().AsSelf().SingleInstance();
+            containerBuilder.RegisterType<CensorshipApi>().As<ICensorshipApi>().SingleInstance();
+
+            return containerBuilder;
         }
     }
 }
--- ZajęciaDI.Tests/Notepad.cs
+++ ZajęciaDI.Tests/Notepad.cs
@@ -29,6 +29,24 @@ namespace ZajęciaDI.Tests
             Assert.That(firstPerson.Contents.Count, Is.EqualTo(1));
         }
 
+        [Test]
+        public void SameTestWithUsingAutofacContainer()
+        {
+            var containerBuilder = Program.CreateBasicContainerBuilder(); // biorę bazowy builder
+            containerBuilder.RegisterType<CensorshipApiMock>().As<ICensorshipApi>(); // nadpisuję rejestrację ICensorshipApi
+            var container = containerBuilder.Build(); // buduję kontener
+
+            var facebookService = container.Resolve<FacebookService>();
+
+            var firstPerson = new Person();
+            facebookService.Register(firstPerson);
+
+            facebookService.PostContent(firstPerson, "Cześć wam");
+            facebookService.PostContent(firstPerson, "Cześć wam głupek");
+
+            Assert.That(firstPerson.Contents.Count, Is.EqualTo(1));
+        }
+
         // Przykładowa funkcja nieprymitywna, a którą można pozostawić jako statyczną - matematyka się nie zmienia
         public static int Power(int baza, int power)
         {
